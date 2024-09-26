f953bbf1eba43ecb2a10dff5b98ec5f452dcb291
claudsaints <claudiodsantoss7@gmail.com>
2024-09-26 12:26:31 -0300

    cache e ajuste para deploy no vercel

--- src/controladores/bd.js
+++ src/controladores/bd.js
@@ -3,17 +3,9 @@ const {Pool} = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Configura o pool de conexão, passando um objeto de configuração 
-// contendo as informações necessárias para se conectar ao BD do PostgreSQL
-// const pool = new Pool({
-//   user: process.env.BDUSUARIO,
-//   host: process.env.BDHOST,
-//   database: process.env.BDNOME,
-//   password: process.env.BDSENHA,
-//   port: process.env.BDPORTA
-// });
 
-const pool = new Pool({connectionString: process.env.BDURI,
+const pool = new Pool({
+  connectionString: process.env.BDURI,
   ssl:{
     rejectUnauthorized: false //ajuste necessário caso esteja utilizando ssl
   }
--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -1,5 +1,7 @@
 const { pool } = require("./bd.js");
 
+
+const cache = {}
 async function criar(req, res) {
   // Desestrutura as propriedades mail e nome
   // do objeto JSON que está no body da requisição HTTTP
@@ -22,21 +24,30 @@ async function criar(req, res) {
 
 async function buscar(req, res) {
   const { mail } = req.body;
+
   if (!mail || mail.length == 0) {
     return res.json({ erro: "Forneça o e-mail" });
   }
+
+
+  if(mail in cache){
+    return res.json({"resposta":cache.mail})
+  }else{
   
-  // Procura na tbusuario o 1o registro que satisfaz as condições
-  let resposta = await pool.query(
-    "SELECT idusuario,mail,nome FROM Usuario WHERE mail=$1 LIMIT 1",
-    [mail]
-  );
-  // Verifica se o usuário existe na tbusuario
-  if (resposta.rowCount > 0) {
-    // Retorna o registro no formato JSON
-    return res.json(resposta.rows[0]);
-  } else {
-    return res.json({erro:"Usuário não cadastrado"});
+    // Procura na tbusuario o 1o registro que satisfaz as condições
+    let resposta = await pool.query(
+      "SELECT idusuario,mail,nome FROM Usuario WHERE mail=$1 LIMIT 1",
+      [mail]
+    );
+    // Verifica se o usuário existe na tbusuario
+    if (resposta.rowCount > 0) {
+      // Retorna o registro no formato JSON
+      cache.mail = resposta.rows[0];
+      console.log(cache)
+      return res.json(resposta.rows[0]);
+    } else {
+      return res.json({erro:"Usuário não cadastrado"});
+    }
   }
 }
 
